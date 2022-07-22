b0fdd4cf72a48c3304dc1cff8ec45ebd1996f835
Kamil D <kamilducz1989@gmail.com>
2022-07-22 20:47:12 +0200

    implementation for task1 added

---
+++ ex_oop/1/Shuffle/CollectionTools.Core/Implementations/ListOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shuffle.Implementations
+{
+    public static class ListOperations
+    {
+        public static List<int> ShuffleItems(List<int> items)
+        {
+            if (items.Count == 0 || items.Count == 1)
+            {
+                return items;
+            }
+
+            var random = new Random();
+            var newShuffledList = new List<int>();
+            for (int i = 0; i < items.Count; i++)
+            {
+                var randomElementInList = random.Next(0, items.Count);
+                newShuffledList.Add(items[randomElementInList]);
+                items.Remove(items[randomElementInList]);
+            }
+            return newShuffledList;
+        }
+    }
+}
