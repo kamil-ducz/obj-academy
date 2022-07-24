c959d9c27fb0a7a6fafbfa839b3cf717d741d805
Kamil D <kamilducz1989@gmail.com>
2022-07-24 15:47:30 +0200

    Initial commit

---
+++ ex_oop/2/OOP.Core/Models/RollingDice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP.Core.Models
+{
+    public class RollingDice
+    {
+        private int sidesCount;
+        private Random random;
+
+        public RollingDice()
+        {
+            random = new Random();
+        }
+
+        public RollingDice(int sidesCount = 6) : this()
+        {
+            this.sidesCount = sidesCount;
+        }
+
+        public int GetSidesCount()
+        {
+            return sidesCount;
+        }
+
+        public int Roll()
+        {
+            return random.Next(1, sidesCount);
+        }
+
+        public string ShowRollResult()
+        {
+            return $"Rolling a dice with { this.sidesCount } sides";
+        }
+    }
+}
