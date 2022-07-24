f50f858944f6b8c9b5deed1e98e00f4187d3764a
Kamil D <kamilducz1989@gmail.com>
2022-07-24 21:08:54 +0200

    Arena alfa implementation

---
+++ ex_oop/2/OOP.Core/Models/Arena.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace OOP.Core.Models
+{
+    public class Arena
+    {
+        public string Name { get; set; }
+        public Hero Hero1 { get; set; }
+        public Hero Hero2 { get; set; }
+        public Hero FirstAttacker { get; set; }
+        public Hero SecondAttacker { get; set; }
+        public string WelcomeMessage { get; set; }
+
+        public Arena(string name, Hero hero1, Hero hero2, Hero firstAttacker, Hero secondAttacker, string welcomeMessage)
+        {
+            Name = name;
+            Hero1 = hero1;
+            Hero2 = hero2;
+            FirstAttacker = firstAttacker;
+            SecondAttacker = secondAttacker;
+            WelcomeMessage = welcomeMessage;
+        }
+
+        private void DisplayArenaWelcomeMessage()
+        {
+            Console.WriteLine($"Welcome to the arena { this.Name }! The fight begins...");
+        }
+
+        public void Render()
+        {
+            Console.Clear();
+
+            DisplayArenaWelcomeMessage();
+            Console.WriteLine("Players are: ");
+
+            Console.WriteLine(Hero1.ShowHealthBar(Hero1.MaxHealth));
+            Console.WriteLine(Hero1.DisplayHeroName());
+            Console.WriteLine("\n");
+            Console.WriteLine(Hero2.ShowHealthBar(Hero2.MaxHealth));
+            Console.WriteLine(Hero2.DisplayHeroName());
+        }
+
+        public void Fight()
+        {
+            var whoIsFirst = new Random().Next(1, 2);
+
+            if (whoIsFirst == 1)
+            {
+                this.FirstAttacker = Hero1;
+                this.SecondAttacker = Hero2;
+            }
+            if (whoIsFirst == 2)
+            {
+                this.FirstAttacker = Hero2;
+                this.SecondAttacker = Hero1;
+            }
+
+            while (Hero1.IsAlive() && Hero2.IsAlive())
+            {
+                FirstAttacker.Attack(SecondAttacker);
+                SecondAttacker.Attack(FirstAttacker);
+            }
+        }
+    }
+}
