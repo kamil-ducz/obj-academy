d2fed7fdffe22375304352728ec9e9c8dd771e92
Kamil D <kamilducz1989@gmail.com>
2022-07-26 21:10:51 +0200

    Warrior and Mage classed added, IHero added

---
+++ ex_oop/2/OOP.Core/Interfaces/IHero.cs
@@ -0,0 +1,13 @@
+﻿using OOP.Core.Models;
+
+namespace OOP.Core.Interfaces
+{
+    public interface IHero
+    {
+        public string DisplayHeroName();
+        public bool IsAlive();
+        public void ShowHealthBar();
+        public void Defend(int hit);
+        public void Attack(Hero enemy);
+    }
+}
