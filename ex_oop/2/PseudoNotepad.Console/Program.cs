8e18e31d632fac96e179d3d731b7b500f850c631
Kamil D <kamilducz1989@gmail.com>
2022-07-27 19:25:45 +0200

    Console model

---
+++ ex_oop/2/PseudoNotepad.Console/Program.cs
@@ -0,0 +1,63 @@
+﻿using PseudoNotepad.Core.Models;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("1. Show all notes titles from the list");
+    Console.WriteLine("2. Open a note from the list");
+    Console.WriteLine("3. Add a note to the list");
+    Console.WriteLine("4. Remove a note from the list");
+    Console.WriteLine("5. Search for a note on the list");
+    Console.WriteLine("6. Close the application");
+
+    var userInput = Console.ReadLine();
+    if (userInput == null 
+        || !int.TryParse(userInput, out int id) 
+        || (Convert.ToInt32(userInput) < 1 
+        || Convert.ToInt32(userInput) > 5))
+    {
+        Console.WriteLine("Wrong input. Try again.");
+        Console.ReadKey();
+        continue;
+    }
+
+    List<Database> notes = new List<Database>();
+
+    switch(Convert.ToInt32(userInput))
+    {
+        case 1:
+            {
+
+                break;
+            }
+
+        case 2:
+            {
+
+                break;
+            }
+
+        case 3:
+            {
+                break;
+            }
+
+        case 4:
+            {
+
+                break;
+            }
+
+        case 5:
+            {
+                Console.WriteLine("Thank you for using the app. See you :)");
+                Environment.Exit(0);
+                break;
+            }
+        
+        default: continue;
+    }
+}
+
+
