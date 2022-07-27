e14e2acc62cc40a655450db74ce482220af9b803
Kamil D <kamilducz1989@gmail.com>
2022-07-27 19:02:50 +0200

    Task 5 basic model

---
+++ ex_oop/2/PseudoNotepad.Core/Models/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PseudoNotepad.Core.Models
+{
+    public class Database
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModified { get; set; }
+
+        public Database(string name, string title, DateTime createdAt, DateTime lastModified)
+        {
+            Name = name;
+            Title = title;
+            CreatedAt = createdAt;
+            LastModified = lastModified;
+        }
+    }
+}
