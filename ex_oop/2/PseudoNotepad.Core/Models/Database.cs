8447d485818926940e0291cec46e8cecb256fb7d
Kamil D <kamilducz1989@gmail.com>
2022-07-27 19:27:06 +0200

    Basic model upgraded

--- ex_oop/2/PseudoNotepad.Core/Models/Database.cs
+++ ex_oop/2/PseudoNotepad.Core/Models/Database.cs
@@ -6,13 +6,15 @@ namespace PseudoNotepad.Core.Models
     {
         public string Name { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public DateTime LastModified { get; set; }
 
-        public Database(string name, string title, DateTime createdAt, DateTime lastModified)
+        public Database(string name, string title, string content, DateTime createdAt, DateTime lastModified)
         {
             Name = name;
             Title = title;
+            Content = content;
             CreatedAt = createdAt;
             LastModified = lastModified;
         }
